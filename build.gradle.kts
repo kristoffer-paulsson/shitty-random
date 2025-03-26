0dff23b5c176804eeabb738ae5a9dc1e2d6196f9
Kristoffer Paulsson <kristoffer.paulsson@talenten.se>
2025-03-26 12:33:10 +0100

    Created installers

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
     id("java")
     id("application")
+    id("distribution")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+val mainClass = "org.example.Main"
 
 repositories {
     mavenCentral()
@@ -21,13 +24,61 @@ tasks.test {
 }
 
 application {
-    mainClass.set("org.example.Main")
+    mainClass.set(mainClass)
     applicationName = "shitty-random"
 }
 
-tasks.register<JavaExec>("runServer") {
-    group = "application"
-    description = "Run the server daemon"
-    mainClass.set("org.example.RandomServer")
-    classpath = sourceSets["main"].runtimeClasspath
+distributions {
+    main {
+        contents {
+            from("src/main/resources") {
+                into("resources")
+            }
+        }
+    }
+}
+
+tasks.register<Exec>("createMacInstaller") {
+    group = "distribution"
+    description = "Create a macOS installer using jpackage"
+    commandLine = listOf(
+        "jpackage",
+        "--type", "pkg",
+        "--input", "build/install/shitty-random",
+        "--dest", "build/installer",
+        "--name", "ShittyRandom",
+        "--main-class", mainClass,
+        "--main-jar", "lib/shitty-random-" + version + ".jar", //"shitty-random.jar",
+        "--icon", "src/main/resources/logo.icns"
+    )
+}
+
+tasks.register<Exec>("createDebInstaller") {
+    group = "distribution"
+    description = "Create a Debian installer using jpackage"
+    commandLine = listOf(
+        "jpackage",
+        "--type", "deb", // or "rpm" for RPM package
+        "--input", "build/install/shitty-random",
+        "--dest", "build/installer",
+        "--name", "ShittyRandom",
+        "--main-class", mainClass,
+        "--main-jar", "lib/shitty-random-" + version + ".jar",
+        "--icon", "src/main/resources/logo.png"
+    )
+}
+
+tasks.register<Exec>("createRpmInstaller") {
+    group = "distribution"
+    description = "Create a RedHat installer using jpackage"
+    commandLine = listOf(
+        "jpackage",
+        "--type", "rpm", // or "rpm" for RPM package
+        "--input", "build/install/shitty-random",
+        "--dest", "build/installer",
+        "--name", "ShittyRandom",
+        "--main-class", mainClass,
+        "--main-jar", "lib/shitty-random-" + version + ".jar",
+        "--icon", "src/main/resources/logo.png"
+    )
 }
